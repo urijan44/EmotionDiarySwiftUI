4c53ff6851081b8bd02247a44ed3b5b56d9a7642
urijan44 <teamsva360@gmail.com>
2022-05-15 15:02:45 +0900

    Add EDUKIt EmotionButton

--- EDUIKit/Package.swift
+++ EDUIKit/Package.swift
@@ -21,9 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EDUIKit",
-            dependencies: []),
-        .testTarget(
-            name: "EDUIKitTests",
-            dependencies: ["EDUIKit"]),
+            dependencies: [])
     ]
 )
--- EDUIKit/Sources/EDUIKit/EmotionButton.swift
+++ EDUIKit/Sources/EDUIKit/EmotionButton.swift
@@ -6,19 +6,58 @@
 //
 
 import SwiftUI
+import Combine
 
 public struct EmotionButton: View {
+
+  //MARK: - Properties
+  var image: () -> Image
+  var title: String
+  @State var countText: String = "0"
+  @Binding var count: Int {
+    didSet {
+      updateCountLabel()
+    }
+  }
+
   public var body: some View {
-    Text("Hello, World!")
+    VStack {
+      image()
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+      HStack{
+        Text(title)
+        Text(countText)
+      }
+    }
+    .onTapGesture {
+      count += 1
+    }
   }
 
-  public init() {
 
+  //MARK: - Methods
+  public init(
+    title: String,
+    count: Binding<Int>,
+    @ViewBuilder image: @escaping () -> Image) {
+      self.title = title
+      self._count = count
+      self.image = image
+    }
+
+  private func updateCountLabel() {
+    countText = count > 1000 ? "999" : "\(count)"
   }
+
 }
 
 struct EmotionButton_Previews: PreviewProvider {
   static var previews: some View {
-    EmotionButton()
+    EmotionButton(
+      title: "즐거워",
+      count: .constant(100)) {
+        Image(systemName: "heart")
+      }
   }
 }
