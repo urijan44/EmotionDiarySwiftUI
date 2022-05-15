5a38d178c35078790c9c3bf318a120e31c290c0a
urijan44 <teamsva360@gmail.com>
2022-05-15 14:19:57 +0900

    init proj

---
+++ EDUIKit/README.md
@@ -0,0 +1,3 @@
+# EDUIKit
+
+A description of this package.
---
+++ EDUIKit/Sources/EDUIKit/EmotionButton.swift
@@ -0,0 +1,24 @@
+//
+//  EmotionButton.swift
+//  
+//
+//  Created by hoseung Lee on 2022/05/15.
+//
+
+import SwiftUI
+
+public struct EmotionButton: View {
+  public var body: some View {
+    Text("Hello, World!")
+  }
+
+  public init() {
+
+  }
+}
+
+struct EmotionButton_Previews: PreviewProvider {
+  static var previews: some View {
+    EmotionButton()
+  }
+}
