88a23a6dd897c9001c9c1589769476f6b17ca91b
urijan44 <teamsva360@gmail.com>
2022-05-15 11:29:24 +0900

    Initial Commit

---
+++ EmotionDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmotionDiary
+//
+//  Created by hoseung Lee on 2022/05/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmotionDiary/EmotionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDiaryApp.swift
+//  EmotionDiary
+//
+//  Created by hoseung Lee on 2022/05/15.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
